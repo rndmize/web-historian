a992c26838a01108c6f6b3ab2529152060ac2b01
Hack Reactor Students <rndmize@gmail.com>
2014-03-19 16:13:57 -0700

    Read from sites.json, do some routing

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,9 +1,15 @@
-var http = require("http");
-var handler = require("./request-handler");
+var http = require('http');
+var handler = require('./request-handler');
+var fs = require('fs');
+
+var sites = JSON.parse(fs.readFileSync('../archives/sites.json'));
+exports.sites = sites;
 
 var port = 8080;
-var ip = "127.0.0.1";
+var ip = '127.0.0.1';
 var server = http.createServer(handler.handleRequest);
-console.log("Listening on http://" + ip + ":" + port);
+console.log('Listening on http://' + ip + ':' + port);
 server.listen(port, ip);
+
+
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,48 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var bs = require('./basic-server.js');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log(bs.sites);
+  res.writeHead(200);
+  if (req.url==='/'){
+    //serve the index
+    fs.readFile('./public/index.html', function(error, data) {
+      if (!error) {
+        res.end(data);
+      }
+    });
+  }
+  else if (req.url === '/styles.css') {
+    fs.readFile('./public/styles.css', function(error, data) {
+      if (!error) {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.end(data);
+      }
+    });
+  }
+  else if (bs.sites[req.url]){
+    /*do stuff*/
+    // If we do, retrieve and serve relevant file
+    console.log('request made for things');
+    fs.readFile('./archives/sites/www.google.com', function(error, data) {
+      if (!error){
+        res.end(data);
+      }
+    });
+  }
+  else {
+    res.writeHead(404);
+    res.end();
+  }
+  //res.end(archive.paths.list);
 };
+
+
+
+// Get url from request
+// Check if we have that url stored
+// If not, go scrap site
+// Discard bad input
