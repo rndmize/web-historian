95cadb9f5a6e15fb50bf503221a0c116ca62a67f
Hack Reactor Students <rndmize@gmail.com>
2014-03-20 10:46:27 -0700

    All functionality complete except cron

--- archives/webscraper.js
+++ archives/webscraper.js
@@ -1,30 +1,22 @@
-// var util = require('util');
-// var pro = require('child_process').exec;
-// var bs = require('./basic-server.js');
 var fs = require('fs');
 var request = require('request');
 
 var sites = JSON.parse(fs.readFileSync('./sites.json'));
 
-// for (var i in sites){
-//   var results = pro('ls');
-//   console.log(results);
-  // if (sites[i]===false){
-  //   console.log(i);
-  //   console.log(sites[i]);
-  //   pro.spawn('wget', sites[i]);
-  //   var args = ['index.html', 'sites/' + sites[i]];
-  //   pro.spawn('mv', args);
-  //   sites[i]=true;
-  // }
-// }
 
+console.log(sites);
+console.log(typeof sites);
 for (var i in sites) {
+  console.log('i an i ', i);
   if (sites[i] === false) {
-    request('http://' + i, function(err, res, body) {
-      console.log(err, res);
-      fs.writeFileSync('./sites/' + i, body);
-    });
+    (function(address) {
+      request('http://' + address, function(err, res, body) {
+        console.log(address);
+        fs.writeFile('./sites/' + address, body, function(){
+          console.log('write file complete ', address);
+        });
+      });
+    }(i));
     sites[i] = true;
     fs.writeFileSync('./sites.json', JSON.stringify(sites));
   }
